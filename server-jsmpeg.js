2d1d4a1de333eac0456fdda3656b4be6cd8cc83a
Cesar Zambrano <cesarzambranod@gmail.com>
2020-01-23 09:38:57 -0300

    first change

---
+++ server-jsmpeg.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.use(express.static(__dirname + '/'));
+//Store all JS and CSS in Scripts folder.
+
+app.use('/', router);
+app.listen(process.env.port || 2400);
+
+console.log('Running at Port 2400');
+
